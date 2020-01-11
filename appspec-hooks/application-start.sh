17465bae49f4f24ab45e3992811c2e4ddc318c3f
KitaharaMugiro <ngthornym@yahoo.co.jp>
2020-01-11 10:32:01 +0900

    nextがインストールされていない問題

--- appspec-hooks/application-start.sh
+++ appspec-hooks/application-start.sh
@@ -3,9 +3,12 @@ cd /var/app/current
 source /home/ec2-user/.nvm/nvm.sh
 
 npm install
-cd ./client-app & npm install
-cd ./server & npm install
-cd ./system-app & npm install
+cd ./client-app 
+npm install
+cd ./server 
+npm install
+cd ./system-app 
+npm install
 
 $(npm bin)/forever start -c "npm run start-dev:server" ./server &
 $(npm bin)/forever start -c "npm run start-dev:system" ./system-app &
