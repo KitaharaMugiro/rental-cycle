2558dac0b7570702e731a836c8cb3278acdc7b2a
KitaharaMugiro <ngthornym@yahoo.co.jp>
2020-01-11 12:29:36 +0900

    kill port

--- appspec-hooks/application-stop.sh
+++ appspec-hooks/application-stop.sh
@@ -5,3 +5,5 @@ source /home/ec2-user/.nvm/nvm.sh
 npm install forever
 $(npm bin)/forever stopall
 kill $(lsof -t -i:8080) || echo "no running on 8080"
+kill $(lsof -t -i:8082) || echo "no running on 8080"
+kill $(lsof -t -i:8999) || echo "no running on 8080"
