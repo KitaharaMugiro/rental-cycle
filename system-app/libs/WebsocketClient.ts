8158ce525e29e25b4463b3c3c14293d0d3c5ced1
KitaharaMugiro <ngthornym@yahoo.co.jp>
2020-01-11 12:16:14 +0900

    debug

--- system-app/libs/WebsocketClient.ts
+++ system-app/libs/WebsocketClient.ts
@@ -6,10 +6,12 @@ const WebSocketURL = "ws://localhost:8999";
 
 export class WebSocketClient {
   test(cb: (text: string) => void) {
+    cb("yo");
     const ws = new WebSocket(WebSocketURL);
-
+    cb("ws created");
     //5秒ごとにsubscribeを送る
     ws.onopen = () => {
+      cb("on open");
       const subscribe: WebSocketRequestProtocol = {
         event: "test",
         data: {
@@ -18,11 +20,13 @@ export class WebSocketClient {
       };
 
       setInterval(() => {
+        cb("send it");
         ws.send(JSON.stringify(subscribe));
       }, 5000);
     };
 
     ws.onmessage = event => {
+      cb("on message");
       const response: WebSocketResponseProtocol = JSON.parse(event.data);
       cb(response.data.message);
     };
