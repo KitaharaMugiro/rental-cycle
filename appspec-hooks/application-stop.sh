eb18e5ebd3823f35c6ab31803e4c9235f6d1a065
KitaharaMugiro <ngthornym@yahoo.co.jp>
2020-01-11 13:01:07 +0900

    dont need forever now

--- appspec-hooks/application-stop.sh
+++ appspec-hooks/application-stop.sh
@@ -2,7 +2,7 @@
 cd /var/app/current
 source /home/ec2-user/.nvm/nvm.sh
 
-npm install forever
+#npm install forever
 $(npm bin)/forever stopall
 kill $(lsof -t -i:8080) || echo "no running on 8080"
 kill $(lsof -t -i:8082) || echo "no running on 8082"
