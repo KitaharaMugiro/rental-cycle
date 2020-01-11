b5cc43baa357facc0e1699149e16d413220cbe1d
KitaharaMugiro <ngthornym@yahoo.co.jp>
2020-01-11 20:24:42 +0900

    buildするように変更

--- appspec-hooks/application-start.sh
+++ appspec-hooks/application-start.sh
@@ -11,10 +11,15 @@ cd ../system-app
 npm install
 cd ../
 
+kill $(lsof -t -i:80) || echo "no running on 80"
 kill $(lsof -t -i:8080) || echo "no running on 8080"
 kill $(lsof -t -i:8082) || echo "no running on 8082"
 kill $(lsof -t -i:8999) || echo "no running on 8999"
 
-$(npm bin)/forever start -c "npm run start-dev:server" ./server &
-$(npm bin)/forever start -c "npm run start-dev:system" ./system-app &
-$(npm bin)/forever start -c "npm run start-dev:client" ./client-app 
+# npm run build:server
+npm run build:system
+npm run build:client
+
+$(npm bin)/forever start -c "npm run start:server" ./server &
+$(npm bin)/forever start -c "npm run start:system" ./system-app &
+$(npm bin)/forever start -c "npm run start:client" ./client-app 
